6d878540113b3640a0645b2c7bd565f5f6d5ef0f
Andre Dargains <andre.dargains@gmail.com>
2015-12-14 20:02:11 +0000

    divs manuais

--- javascript.js
+++ javascript.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
-    $("button").click(function(){
-        $("p").css("color", "yellow");
-    });
+	$("button").click(function(){
+		var i = 0;
+		while(i<64){
+		$("#container").append("<div class='box'></div>");
+	}
+	});
+
+	$(".box").hover(
+		function(){
+        	$(this).css("background","red");
+    	},
+    	function(){
+        	$(this).css("background","blue");
+    	}
+    );
 });
