4367b3ff31615c40e5391afe5428a1fac47aad5e
Andre Dargains <andre.dargains@gmail.com>
2015-12-14 22:33:20 +0000

    cores adicionadas

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+var cor;
+
 $(document).ready(function(){
 	$("#comecar").click(function(){
 		var i = 0;
@@ -9,9 +11,15 @@ $(document).ready(function(){
 
 	$("#limpar").click(function(){
 		$(".box").css("background","white");
-	})
+	});
+
+	$(".cor").click(function(){
+		$(this).css("border","1px solid black");
+		cor = $(this).attr("id");
+	});
+
 });
 
 $(document).on("mouseenter",".box", function(){
-	$(this).css("background","black");
+	$(this).css("background",cor);
 });
